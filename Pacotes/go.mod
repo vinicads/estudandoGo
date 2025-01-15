ae7d3953f1e9ce1f26224540851bb94e70f26258
Vinicius Augusto <vinicim.augusto@outlook.com>
2025-01-15 14:32:06 -0300

    Manipulando dependencias externas

--- Pacotes/go.mod
+++ Pacotes/go.mod
@@ -1,3 +1,5 @@
 module modulo
 
 go 1.23.4
+
+require github.com/badoux/checkmail v1.2.4 // indirect
--- Pacotes/main.go
+++ Pacotes/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+	 "github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main.")
 	auxiliar.Write()
+	erro := checkmail.ValidateFormat("viniciusadstrab@outlook.com")
+	fmt.Println(erro)
 }
