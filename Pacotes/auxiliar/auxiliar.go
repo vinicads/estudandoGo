63eaf739d8c5b8fbc3bd9c692517329d7c1cccca
Vinicius Augusto <vinicim.augusto@outlook.com>
2025-01-15 14:22:39 -0300

    Aprendendo sobre os pacotes do GO

---
+++ Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Escreve uma mensagem simples na tela
+func Write() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+	write2()
+}
---
+++ Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// Funcao que so consigo usar no mesmo package
+func write2() {
+	fmt.Println("Escrevendo pela funcao Write2")
+}
